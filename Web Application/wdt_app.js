47fd80a7850bae3c2bfe3ee38783ba4d4dcbac7d
Karen Beate <karen.beate@outlook.com>
2023-05-04 19:58:25 +0200

    modal for prompt, shortening

--- Web Application/wdt_app.js
+++ Web Application/wdt_app.js
@@ -135,22 +135,26 @@ function getSelectedObject(){
     return obj;
 }
 
+function getMinutes(){
+    $("#staffOutToast").toast("hide");
+    $("#modalMinutesAway").modal("show");
+}
+
 //staff goin out for break/awaytime from workday.
 function staffOut(){
-    $("#staffOutToast").toast("hide");
     let obj = getSelectedObject();
     let hours = parseInt(digitalClock().slice(0,2));
     const minutes = parseInt(digitalClock().slice(3,5));
-    let away = prompt("Enter estimated away time in minutes:","enter minutes")
+    let away = $("#minutesAway").val();
     obj.status = "Break";
     obj.outTime = (minutes < 10) ? (hours + ":0" + minutes) : (hours + ":" + minutes);
         let hour = Math.floor(+away/60);
         let minute = (+away-(hour*60));
     obj.duration = (+away < 60) ?  (away + "min") : (hour + "hr " + minute + "min");
-        let x = +away + minutes;
-        while(x >= 60){x -= 60;hours += 1; obj.expectedReturn = (x <= 10) ? (hours + ":0" + x) : (hours + ":" + x);}       
-    obj.expectedReturn = (hours + ":" + x);
-    obj.minutesaway = away; 
+    let x = +away + minutes;
+    if(x >= 60){x -= 60;hours += 1; obj.expectedReturn = (x <= 10) ? (hours + ":0" + x) : (hours + ":" + x);}
+    else{obj.expectedReturn = (x <= 10) ? (hours + ":0" + x) : (hours + ":" + x);}
+    obj.minutesaway = away;
         updateTable(obj);                   
 }
 
@@ -193,7 +197,7 @@ function updateTable(obj){
     }
 }
 
-//Clearing timeout if staff clicked In before expecte return.
+//Clearing timeout if staff clicked In before expected return.
 const noStaffTimeout = () => clearTimeout(staffTimeout); 
 
 //checking for only letter input.
@@ -241,7 +245,6 @@ function addDelivery(obj){
     row.insertCell(0).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-car-front-fill" viewBox="0 0 16 16">
     <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17 1.247 0 3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/>
   </svg>`;
-    //Fiat 500 by Icons8
     }
     else{
     row.insertCell(0).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bicycle" viewBox="0 0 16 16">
